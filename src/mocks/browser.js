6f7b3649330cd1d99c6a3490bdd45e986c0c77b2
Navin Kumar Singh <113549400+NavinSingh1@users.noreply.github.com>
2026-03-30 12:06:13 +0530

    Delete src directory

--- src/mocks/browser.js
+++
@@ -1,9 +0,0 @@
-// ─────────────────────────────────────────────
-// src/mocks/browser.js
-// MSW Browser Worker Setup
-// ─────────────────────────────────────────────
-import { setupWorker } from "msw/browser";
-import { handlers } from "./handlers";
-
-// Create the worker with all handlers
-export const worker = setupWorker(...handlers);
